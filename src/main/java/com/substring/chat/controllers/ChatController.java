efd25a5a834c1cf6fa19f3f1767955d0b63d88f9
Rohith Gowda <rohithrgowda23@gmail.comexample.com>
2026-06-07 12:42:34 +0530

    Fix CORS origin

--- src/main/java/com/substring/chat/controllers/ChatController.java
+++ src/main/java/com/substring/chat/controllers/ChatController.java
@@ -20,7 +20,7 @@ import java.time.ZonedDateTime;
 @Controller
 @CrossOrigin(origins = {
         "http://localhost:5173",
-        "https://chat-frontend-eight-psi.vercel.app/"
+        "https://chat-frontend-eight-psi.vercel.app"
 })
 public class ChatController {
 
